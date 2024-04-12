90defc5c9c8235f362be09bc4009a9c4cf519c69
Juan Garate <garatejb@gmail.com>
2024-04-12 05:24:47 -0700

    DS_PI2

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ from math import factorial
 from scipy import stats
 
 
-car_data = pd.read_csv('vehicles_us.csv', sep=',') # leer los datos   
-print (car_data)
+car_data = pd.read_csv('vehicles_us.csv', sep=',') 
+#print (car_data)
+dfhl = pd.read_csv('dfhl', sep=',')
 
 st.header('Curso Data Science - Proyecto Individual Nro. 2: DATA ANALYTICS')
 
 hist_button = st.button('Construir histograma')  # crear un botón
-
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
@@ -25,7 +25,6 @@ if hist_button:  # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 scatter_button = st.button('Construir grafico de dispersion')
-
 if scatter_button:
     # escribir un mensaje
     st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
@@ -33,3 +32,13 @@ if scatter_button:
     fig = px.scatter(car_data, x="odometer", y="price")
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+acusado_victima_button = st.button('Mostrar gráfico sunburst de acusados y víctimas')
+if acusado_victima_button:
+    df_sun = dfhl.groupby(['acusado', 'victima'])['n_victimas'].sum().reset_index()
+    fig = px.sunburst(df_sun, path=['acusado', 'victima'], values='n_victimas', 
+                  hover_data=['n_victimas'],  # Mostrar valores al pasar el mouse
+                  title='Gráfico Sunburst con Valores de n_victimas')
+
+fig.update_layout(width=800, height=600)
+fig.show()
